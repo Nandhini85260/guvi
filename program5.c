2c1428118374047d4748f236cb33465041b635a6
Nandhini85260 <nandhini85260@gmail.com>
2016-10-24 15:43:25 +0530

    Create program5.c

---
+++ program5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\n greatest Number");
+    printf("\n Enter the three numbers:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    {
+        printf("\n %d is greatest",a);
+    }
+    else 
+    if(b>c)
+    {
+        printf("\n%d is greatest",b);
+    }
+    else
+    {
+        printf("\n %d is greatest",c);
+    }
+    return 0;
+}
